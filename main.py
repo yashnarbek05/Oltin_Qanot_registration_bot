c03665aed06936286de1226b0d8b0e8373f7b574
Yashnarbek <yashnarbeksattorov15@gmail.com>
2024-12-27 03:14:23 -0800

    sheet service works :))

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from image.service import prepare_badge
 from sheet.service import get_values_from_sheet
 
 if __name__ == '__main__':
-    #print(prepare_badge("yashnarbek","sattorov", "26.12.2024 2:19:08","10", "images/photo_2024-10-17_22-16-26.jpg"))
-
-    print(get_values_from_sheet())
+    print(get_values_from_sheet()[1][2])
--- sheet/service.py
+++ sheet/service.py
@@ -7,8 +7,10 @@
 # print(df.get("1. F. I. SH. (Pasportdagidek to'liq yozing)"))
 #
 # print(df.keys())
+import pandas as pd
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 from config import GOOGLE_SHEET_URL, KEYS_PATH
 
@@ -16,20 +18,30 @@ from config import GOOGLE_SHEET_URL, KEYS_PATH
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 SERVICE_ACCOUNT_FILE = KEYS_PATH
 
-creds = None
-creds = creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-# The ID and range of a sample spreadsheet.
+# The ID of sheet
 SAMPLE_SPREADSHEET_ID = GOOGLE_SHEET_URL
 
 service = build("sheets", "v4", credentials=creds)
-
 sheet = service.spreadsheets()
 
 
 def get_values_from_sheet():
+    try:
+        result = sheet.values().get(
+            spreadsheetId=SAMPLE_SPREADSHEET_ID,
+            range="sheet 1"
+        ).execute()
 
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range="sheet 1").execute()
+        values = result.get("values", [])
 
-    return result
+        return values
+
+    except HttpError as err:
+        print(f"HTTP Error: {err}")
+        return []
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return []
