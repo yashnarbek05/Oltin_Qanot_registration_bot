3e0c31c9de76efc4c6263cb1779ea4048811aad6
Yashnarbek <yashnarbeksattorov15@gmail.com>
2024-12-27 02:31:11 -0800

    sheet service works :)))

--- mian.py
+++ mian.py
@@ -1,4 +1,7 @@
 from image.service import prepare_badge
+from sheet.service import get_values_from_sheet
 
 if __name__ == '__main__':
-    print(prepare_badge("yashnarbek","sattorov", "26.12.2024 2:19:08","10", "images/photo_2024-10-17_22-16-26.jpg"))
+    #print(prepare_badge("yashnarbek","sattorov", "26.12.2024 2:19:08","10", "images/photo_2024-10-17_22-16-26.jpg"))
+
+    print(get_values_from_sheet())
--- sheet/service.py
+++ sheet/service.py
@@ -1,21 +1,35 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
+# import pandas as pd
+#
+# from config import GOOGLE_SHEET_URL
+#
+# df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{GOOGLE_SHEET_URL}/export?format=csv")
+#
+# print(df.get("1. F. I. SH. (Pasportdagidek to'liq yozing)"))
+#
+# print(df.keys())
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
 
-# Setup credentials
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("path/to/your/jsonfile.json", scope)
-client = gspread.authorize(creds)
+from config import GOOGLE_SHEET_URL, KEYS_PATH
 
-# Open sheet by URL or title
-spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/your-sheet-id")
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SERVICE_ACCOUNT_FILE = KEYS_PATH
 
-# Select worksheet
-worksheet = spreadsheet.worksheet("Sheet1")
+creds = None
+creds = creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-# Get data
-data = worksheet.get_all_records()
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = GOOGLE_SHEET_URL
 
-# Convert to DataFrame
-df = pd.DataFrame(data)
-print(df.head())
+service = build("sheets", "v4", credentials=creds)
+
+sheet = service.spreadsheets()
+
+
+def get_values_from_sheet():
+
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range="sheet 1").execute()
+
+    return result
